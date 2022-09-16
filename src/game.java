b85cc177ce4968c703576181e6f1f085b9a226f2
avivv <avivb60@gmail.com>
2022-09-16 13:31:03 +0300

    first vommmit

---
+++ src/game.java
@@ -0,0 +1,84 @@
+import java.util.*;
+class game
+{
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+        int game_level = 0;
+        int score = 0;
+        System.out.println("select level 1-3 of the game!");
+
+
+        boolean game_level_val = true;
+        while(game_level_val) {
+        try {
+            game_level = 0;
+            game_level = input.nextInt();
+            game_level_val = false;
+
+        } catch (Exception e) {
+            System.out.println("select NUMBER 1, 2 or 3 Please. thank you");
+            game_level_val = true;
+            input.next();
+        }}
+
+        if (game_level == 1) {
+            System.out.println("ok, we go new!");
+            while (1 == 1) {
+                System.out.println("\n" + "\n" + "\n" + "\n" + "\n" + "\n" + "\n");
+                System.out.println("To escape this nightmare just write \"Exit\"");
+                System.out.println("scorecard: " + score);
+
+                int num1 = random.nextInt(100);
+                int num2 = random.nextInt(100);
+                System.out.println("try this one:");
+                System.out.println(num1 + " + " + num2 + "?");
+                int unserNum = unserCheker();
+//                int unser = input.nextInt();
+                if (unserNum == (num1 + num2)) {
+                    System.out.println("You are equal to God");
+                    System.out.println("i gonna give u 2 points: ");
+                    score = score + 2;
+
+                } else {
+                    System.out.println("are you stupid?");
+                    System.out.println("i gonna take take back 5 points...");
+                    score = score - 5;
+                }
+            }
+        } else {
+            System.out.println("Why the fu*k don't you pick 1? Are you a masochist?");
+        }
+    }
+
+    public static int unserCheker(){
+//        System.out.println("חזירים");
+        Scanner input = new Scanner(System.in);
+        int unser = 0;
+        boolean game_unser_valid = false;
+        while(!(game_unser_valid)) {
+
+            try {
+                String user_Unser = input.nextLine();
+                user_Unser.toLowerCase();
+                if (Objects.equals(user_Unser, "exit")){
+                    System.out.println("good by my old friend");
+                    System.exit(0);
+                }
+                else {
+                    unser = Integer.parseInt(user_Unser);
+                    game_unser_valid = true;
+                }
+
+            } catch (Exception e) {
+                System.out.println("unser NUMBER Please. thank you \nTo escape this nightmare just write \"Exit\"");
+                game_unser_valid = false;
+
+            }}
+
+        return unser;
+    }
+
+}
+
